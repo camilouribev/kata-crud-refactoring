13f955dda04f3883e6b0663f9d57644af06dfe9e
Camilo U <cauribeva@unal.edu.co>
2021-09-15 13:02:04 -0500

    Modificados empaquetamientos y endpoints en el back

--- back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
+++ back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
@@ -1,26 +1,29 @@
-package co.com.sofka.crud;
+package co.com.sofka.crud.controllers;
 
+import co.com.sofka.crud.entities.Todo;
+import co.com.sofka.crud.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(value = "api")
 public class TodoController {
 
     @Autowired
     private TodoService service;
 
-    @GetMapping(value = "api/todos")
+    @GetMapping(value = "/todos")
     public Iterable<Todo> list(){
         return service.list();
     }
     
-    @PostMapping(value = "api/todo")
+    @PostMapping(value = "/todo")
     public Todo save(@RequestBody Todo todo){
         return service.save(todo);
     }
 
-    @PutMapping(value = "api/todo")
+    @PutMapping(value = "/todo")
     public Todo update(@RequestBody Todo todo){
         if(todo.getId() != null){
             return service.save(todo);
@@ -28,12 +31,12 @@ public class TodoController {
         throw new RuntimeException("No existe el id para actualziar");
     }
 
-    @DeleteMapping(value = "api/{id}/todo")
+    @DeleteMapping(value = "/{id}/todo")
     public void delete(@PathVariable("id")Long id){
         service.delete(id);
     }
 
-    @GetMapping(value = "api/{id}/todo")
+    @GetMapping(value = "/{id}/todo")
     public Todo get(@PathVariable("id") Long id){
         return service.get(id);
     }
